7d335eee2b2144bd72e91a92ac3259119db09c6f
segelfartyg <samuel.swaren@outlook.com>
2024-08-25 19:23:35 +0200

    Add: fonts, initial home page

---
+++ Fiesta.Frontend/src/routes/home/+page.svelte
@@ -0,0 +1,50 @@
+<div class="welcome">
+
+    <div class="container">
+    <h1>Snake Fiesta</h1>
+    <p>Snake Fiesta is an online snake game made for wasting time.</p>
+    <p>Login to start customizing your snake and enter the fiesta.</p>
+    <p>Remember to follow the <a href="/rules">rules</a> while inside the fiesta. The most important rule is to have fun, so try not to ruin peoples time in the fiesta, you are (hopefully) not alone in there. </p>
+    <input type="text">
+    <button>START PLAYING</button>
+    </div>
+</div>
+
+
+<style>
+    .welcome{
+        display: flex;
+        flex-direction: column;
+    }
+    .container{
+        width:400px;
+        margin:auto;
+        background:rgb(255, 255, 255);
+        display:flex;
+        text-align: center;
+        flex-direction: column;
+    }
+
+h1 {
+/* font-family: "chantal", sans-serif;
+font-weight: 300;
+font-style: normal;
+font-family: "chantal", sans-serif;
+font-weight: 300;
+font-style: italic;
+font-family: "chantal", sans-serif;
+font-weight: 600;
+font-style: normal; */
+font-family: "chantal", sans-serif;
+font-weight: 600;
+font-style: italic; 
+
+}
+
+p{
+    font-family: "john-doe", sans-serif;
+font-weight: 400;
+font-style: normal;
+}
+
+</style>
